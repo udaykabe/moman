85f48ccd847856ad5f52278d24fb32e891bb1320
bolton.nick <bolton.nick@b25f765c-132c-11df-a685-e9edad190965>
2010-02-12 00:08:02 +0000

    bye

--- eclipse/eclipse/plugins/moman_1.0.0.201001250633/src/main/java/net/deuce/moman/model/EntityListener.java
+++
@@ -1,9 +0,0 @@
-package net.deuce.moman.model;
-
-
-public interface EntityListener<E extends MomanEntity>  {
-	
-	public void entityAdded(EntityEvent<E> event);
-	public void entityRemoved(EntityEvent<E> event);
-	public void entityChanged(EntityEvent<E> event);
-}
